f5b37c523c911cef11a0d5bf2285d8e57bbb6273
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-01-22 19:09:37 +0100

    Add files via upload

---
+++ Cetvrti.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPZ3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n3.4");
+            Console.WriteLine("Upišite neki pozitivni broj:");
+            string s34 = Console.ReadLine();
+            int x34 = Convert.ToInt32(s34);
+            for (int i = 1; i < x34; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j);
+                Console.WriteLine();
+            }
+        
+
+    }
+}
+    }
+
